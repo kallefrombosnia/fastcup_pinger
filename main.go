700904985b0ae3aa0e86c07732920bac34b8434a
kallefrombosnia <kallegowild@gmail.com>
2021-10-26 23:21:35 +0200

    json parse

--- main.go
+++ main.go
@@ -1,9 +1,27 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/kallefrombosnia/fc_pinger/root"
 )
 
 func main() {
+
+	// Print welcome messages
 	root.Welcome()
+
+	// Check if we have remotes file
+	if !root.CheckForFile() {
+
+		// If doesn exists download file from github
+		root.DownloadRegions()
+
+		fmt.Println("Downloading regions.json \n\r")
+	}
+
+	fmt.Println("Try to load regions...\n\r")
+
+	regions := root.GetRegions()
+	
 }
